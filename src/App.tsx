4a078717276604a50ce253ee4f31ef180c701882
fitfusionhq1 <fit.fusionhq1@gmail.com>
2026-02-04 16:54:00 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import HeroSection from "@/components/HeroSection";
-import Gallery from "@/components/Gallery";
+import GallerySection from "@/components/GallerySection";
 import EventDetails from "@/components/EventDetails";
 import RSVPSection from "@/components/RSVPSection";
 
