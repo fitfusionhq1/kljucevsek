47eb4431f39ca5770083a482853ff897853de2b0
fitfusionhq1 <fit.fusionhq1@gmail.com>
2026-02-04 16:51:51 +0100

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -4,7 +4,7 @@ import EventDetails from '@/components/EventDetails';
 import GiftRegistry from '@/components/GiftRegistry';
 import RSVPSection from '@/components/RSVPSection';
 import Footer from '@/components/Footer';
-
+import GallerySection from "@/components/GallerySaction";
 const Index = () => {
   return (
     <div className="min-h-screen bg-background">
