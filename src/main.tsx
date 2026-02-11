f08afeecaacf09c492f37918c1a445499b31e8cf
fitfusionhq1 <fit.fusionhq1@gmail.com>
2026-02-11 10:46:20 +0000

    modified:   src/main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { GuestProvider } from "@/lib/GuestContext";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <GuestProvider>
-      <App />
-    </GuestProvider>
-  </React.StrictMode>
+  <GuestProvider>
+    <App />
+  </GuestProvider>
 );
