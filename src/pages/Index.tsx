d222a673d1dc924c9db9c809d636752d93114f30
fitfusionhq1 <fit.fusionhq1@gmail.com>
2026-02-11 09:36:37 +0100

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -5,10 +5,13 @@ import GiftRegistry from '@/components/GiftRegistry';
 import RSVPSection from '@/components/RSVPSection';
 import Footer from '@/components/Footer';
 import GallerySection from "@/components/GallerySection";
+import IconNav from "@/components/IconNav";
+
 const Index = () => {
   return (
     <div className="min-h-screen bg-background">
       <HeroSection />
+      <IconNav />
       <CountdownSection />
       <GallerySection />
       <EventDetails />
